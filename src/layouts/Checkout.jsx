e0f5a48fe4a0a1ce30ab0c685c038d88ee704193
Anastasia <udachinanastasia@gmail.com>
2026-04-24 10:50:57 +0200

    seven final commit

--- src/layouts/Checkout.jsx
+++ src/layouts/Checkout.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import style from './Checkout.module.css'
 import {Button} from '@mui/material'
+import { Link } from 'react-router-dom'
 export default function Checkout() {
   return (
     <div className={style.container}>
       <h1>Amazing Discounts on Pets Products!</h1>
+      <Link to='/products/sale'>
       <Button variant="contained">Check out</Button>
+      </Link>
     </div>
   )
 }
